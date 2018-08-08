0ea66b52ccc2eabe8e050b5c804ecc884b3db26d
shuni <shun@qinliannet.com>
2018-08-08 18:33:45 +0800

    新建星座运势详情页面和星座运势plist文件

---
+++ ConstellationHouse/Main/Controller/CHFortuneDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  CHFortuneDetailVC.h
+//  ConstellationHouse
+//
+//  Created by shuni on 2018/8/8.
+//  Copyright © 2018年 shuni. All rights reserved.
+//
+
+#import "CHBaseVC.h"
+
+@interface CHFortuneDetailVC : CHBaseVC
+
+@end
---
+++ ConstellationHouse/Main/Controller/CHFortuneDetailVC.m
@@ -0,0 +1,27 @@
+//
+//  CHFortuneDetailVC.m
+//  ConstellationHouse
+//
+//  Created by shuni on 2018/8/8.
+//  Copyright © 2018年 shuni. All rights reserved.
+//
+
+#import "CHFortuneDetailVC.h"
+
+@interface CHFortuneDetailVC ()
+
+@end
+
+@implementation CHFortuneDetailVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"星座运势详情";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
